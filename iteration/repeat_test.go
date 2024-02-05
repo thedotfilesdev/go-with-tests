ba9757b8b118d5b9cac1eabcfcef71eb7bfd60a6
Patryk Błaziński <blazinski.patryk@gmail.com>
2024-02-05 21:30:32 +0100

    feat(): finish chapter iteration

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,12 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := RepeatCharacter("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		RepeatCharacter("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := RepeatCharacter("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
